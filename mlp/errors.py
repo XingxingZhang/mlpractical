40b3fb0b01c67331797237689763899a00c3a625
XingxingZhang <zxxpku@gmail.com>
2016-10-08 16:22:31 +0100

    finished lab2

--- mlp/errors.py
+++ mlp/errors.py
@@ -25,7 +25,9 @@ class SumOfSquaredDiffsError(object):
         Returns:
             Scalar error function value.
         """
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        diff = outputs - targets
+        return (diff * diff).sum() / 2 / targets.shape[0] 
 
     def grad(self, outputs, targets):
         """Calculates gradient of error function with respect to outputs.
@@ -38,7 +40,8 @@ class SumOfSquaredDiffsError(object):
             Gradient of error function with respect to outputs. This should be
             an array of shape (batch_size, output_dim).
         """
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        return (outputs - targets) / targets.shape[0]
 
     def __repr__(self):
         return 'SumOfSquaredDiffsError'
--- mlp/layers.py
+++ mlp/layers.py
@@ -113,7 +113,8 @@ class AffineLayer(LayerWithParameters):
         Returns:
             outputs: Array of layer outputs of shape (batch_size, output_dim).
         """
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        return inputs.dot(self.weigths.T) + self.biases
 
     def grads_wrt_params(self, inputs, grads_wrt_outputs):
         """Calculates gradients with respect to layer parameters.
@@ -127,7 +128,8 @@ class AffineLayer(LayerWithParameters):
             list of arrays of gradients with respect to the layer parameters
             `[grads_wrt_weights, grads_wrt_biases]`.
         """
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        return np.dot(grads_wrt_outputs.T, inputs), grads_wrt_outputs.sum(0)
 
     @property
     def params(self):
